9f59bc6080d4d7944c18d2154924f7a800e523d5
nateazevedo <nathanielazevedo@hotmail.com>
2025-11-09 12:17:19 -0500

    fix vercel build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    target: 'esnext',
+    minify: 'esbuild',
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
